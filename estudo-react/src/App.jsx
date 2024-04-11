d45edca1ca12beebb5483be49e8b2fbba5b2f410
mariah.silveira <mariah.silveira@uerj.br>
2024-04-11 16:54:46 -0300

    Alterações na página

--- estudo-react/src/App.jsx
+++ estudo-react/src/App.jsx
@@ -4,8 +4,9 @@ function App() {
  
   return (
     <>
+    <img src = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1024px-React_Logo_SVG.svg.png" height = "100" width = "100"></img>
     <h1>Estudo React</h1>
-    <h2>Bem vindo ao estudo-react 1.0<br></br></h2>
+    <h2>Bem vindo ao estudo-react 1.0</h2>
     <div>
       <label>
         Nome:
@@ -42,6 +43,7 @@ function App() {
         />
       </label>
     </div>
+    <footer><br></br>Created by Leonardo</footer>
     </>
   )
 }
