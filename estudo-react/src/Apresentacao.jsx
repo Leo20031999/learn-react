45098df1330a93748445c04e363da7991cff97cc
mariah.silveira <mariah.silveira@uerj.br>
2024-04-12 16:21:26 -0300

    Mudancas na Apresentacao

--- estudo-react/src/Apresentacao.jsx
+++ estudo-react/src/Apresentacao.jsx
@@ -1,26 +1,30 @@
-import './App.css'
+
 import React from 'react'
-import {BrowserRouter, Routes, Route, useNavigate} from 'react-router-dom'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 import App from './App.jsx'
 import Informacoes from './Informacoes.jsx'
 
 
 function Apresentacao() {
-  //const navigate = useNavigate();
-  return (
+
+return (
     <>
-      <h1>Bem vindo</h1>
-      <BrowserRouter>
-        <Routes>
-            <Route path="/login" element={<App />} />
-            <Route path="/informacoes" element={<Informacoes />} />
-        </Routes>
-      </BrowserRouter>
-      <button onClick={() => navigate('/login')}>Login</button><br/><br/>
-      <button onClick={() => navigate('/informacoes')}>Mais informações</button>
+        <h1>Bem vindo</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/login" element={<App />} />
+                <Route path="/informacoes" element={<Informacoes />} />
+            </Routes>
+        </BrowserRouter>
+        {window.location.pathname === '/' && (
+            <>
+                <button onClick={() => window.location.href = '/login'}>Login</button><br/><br/>
+                <button onClick={() => window.location.href = '/informacoes'}>Mais informações</button>
+            </>
+        )}
     </>
-  )
+)
 }
  
 export default Apresentacao
